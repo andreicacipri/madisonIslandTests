1284918859f02f87a246fb38c73450b2fd304071
andreicacipri <cipri9412@gmail.com>
2019-02-06 21:33:04 +0200

    Add to cart test

---
+++ src/test/java/org/fasttrackit/ShoppingCartTest.java
@@ -0,0 +1,40 @@
+package org.fasttrackit;
+
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+public class ShoppingCartTest {
+    @Test
+    public void addToCartFromSearchResultsTest(){
+        System.setProperty("webdriver.chrome.driver", "C:\\chromedriver.exe");
+
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://fasttrackit.org/selenium-test/");
+        String keyword = "vase";
+        driver.findElement(By.id("search")).sendKeys(keyword);
+        driver.findElement(By.tagName("button")).click();
+
+        String productToCart ="Herald Glass Vase";
+        driver.findElement(By.xpath(
+                "//div[@class='product-info'and .//a[text()='"+productToCart+"']]//button[@title='Add to Cart']")).click();
+
+       String cart = driver.findElement(By.className("success-msg")).getText();
+
+       assertThat("Succes messege is not display",cart,is((""+productToCart+" was added to your shopping cart.")));
+        String product = driver.findElement(By.xpath("//tr[@class='first last odd']//h2[@class='product-name']//a[text()='"+productToCart+"']")).getText();
+        assertThat("Succes messege is not display",product,is(productToCart.toUpperCase()));
+
+        WebElement productNameInCart = driver.findElement(By.xpath(("//table[@id='shopping-cart-table']//a[text()='"+productToCart+"']")));
+        assertThat("prod",productNameInCart.isDisplayed());
+
+
+
+
+    }
+}
