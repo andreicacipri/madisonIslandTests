bb637a83bcd33fdb909457a39f2570f2411b8594
andreicacipri <cipri9412@gmail.com>
2019-02-20 21:21:23 +0200

    studied pageobject

--- src/test/java/org/fasttrackit/ShoppingCartTest.java
+++ src/test/java/org/fasttrackit/ShoppingCartTest.java
@@ -1,11 +1,13 @@
 package org.fasttrackit;
 
+import org.fasttrackit.pageobjects.Header;
+import org.fasttrackit.pageobjects.ProductsGrid;
 import org.junit.Test;
 import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.PageFactory;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -13,25 +15,26 @@ import static org.hamcrest.MatcherAssert.assertThat;
 public class ShoppingCartTest {
     @Test
     public void addToCartFromSearchResultsTest(){
-        System.setProperty("webdriver.chrome.driver", "C:\\chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver",AppConfig.getChromeDriverPath());
 
         WebDriver driver = new ChromeDriver();
-        driver.get("https://fasttrackit.org/selenium-test/");
+        driver.get(AppConfig.getSiteUrl());
+        Header header = PageFactory.initElements(driver,Header.class);
         String keyword = "vase";
-        driver.findElement(By.className("input-text")).sendKeys(keyword+ Keys.ENTER);
+        header.search(keyword);
         System.out.println("Pressed enter in search field. ");
 
-        String productToCart ="Herald Glass Vase";
-        driver.findElement(By.xpath(
-                "//div[@class='product-info'and .//a[text()='"+productToCart+"']]//button[@title='Add to Cart']")).click();
+        String productName ="Herald Glass Vase";
+        ProductsGrid productsGrid = PageFactory.initElements(driver,ProductsGrid.class);
+        productsGrid.addProductToCart(productName,driver);
 
        String cart = driver.findElement(By.className("success-msg")).getText();
 
-       assertThat("Succes messege is not display",cart,is((""+productToCart+" was added to your shopping cart.")));
-        String product = driver.findElement(By.xpath("//tr[@class='first last odd']//h2[@class='product-name']//a[text()='"+productToCart+"']")).getText();
-        assertThat("Succes messege is not display",product,is(productToCart.toUpperCase()));
+       assertThat("Succes messege is not display",cart,is((""+productName+" was added to your shopping cart.")));
+        String product = driver.findElement(By.xpath("//tr[@class='first last odd']//h2[@class='product-name']//a[text()='"+productName+"']")).getText();
+        assertThat("Succes messege is not display",product,is(productName.toUpperCase()));
 
-        WebElement productNameInCart = driver.findElement(By.xpath(("//table[@id='shopping-cart-table']//a[text()='"+productToCart+"']")));
+        WebElement productNameInCart = driver.findElement(By.xpath(("//table[@id='shopping-cart-table']//a[text()='"+productName+"']")));
         assertThat("prod",productNameInCart.isDisplayed());
 
 
